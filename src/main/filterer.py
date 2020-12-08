5648e00ad272437569936e0fe620960096100bdc
nmm131 <nmm131@gmail.com>
2020-12-08 15:59:23 -0500

    Updated 'filterer.py' and 'lister.py'

--- src/main/filterer.py
+++ src/main/filterer.py
@@ -1,41 +1,30 @@
 # Created by Leon Hunter at 12:10 PM 12/08/2020
 class Filterer(object):
     def remove_characters(self, string_to_remove_from, characters_to_remove):
-        ''' TODO - Implement solution
-        Given 2 arguments:
-            `string_to_remove_from`, representative of the string to remove the characters from
-            `characters_to_remove`, representative of the string of characters that should be removed from the `string_to_remove_from`
-        return
-            (case sensitive)
-            a string with each character in the `characters_to_remove` removed from the string
-        '''
+        result = str(string_to_remove_from)
+        for char in string_to_remove_from:
+            if char in characters_to_remove:
+                result = result.replace(char, '')
 
+        return result
 
 
     def remove_vowels(self, string_to_remove_from):
-        ''' TODO - Implement solution
-        Given 1 arguments:
-            `string_to_remove_from`, representative of the string to remove the characters from
-        return
-            (case insensitive)
-            a string with each character in the list `['a','e',i','o','u']` removed from the string
-        '''
         result = ""
         vowels = ["a", "e", "i", "o", "u"]
         for char in string_to_remove_from:
-            if char not in vowels or char.upper() not in vowels:
+            if char.lower() not in vowels:
                 result += char
 
         return result
 
 
     def remove_consonants(self, string_to_remove_from):
-        ''' TODO - Implement solution
-        Given 1 arguments:
-            `string_to_remove_from`, representative of the string to remove the characters from
-        return
-            (case insensitive)
-            a string with each character in the list `['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']` removed from the string
-        '''
-        return None
+        result = ""
+        constants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+        for char in string_to_remove_from:
+            if char.lower() not in constants:
+                result += char
+
+        return result
 
--- src/main/lister.py
+++ src/main/lister.py
@@ -1,36 +1,23 @@
 # Created by Leon Hunter at 12:10 PM 12/08/2020
 class Lister(object):
     def get_integer_list(self, start, stop, step):
-        ''' TODO - Implement solution
-        Given 3 integers,
-            `start`, `stop`, and `step`
-        return a list of all integers
-            between `start`,
-            incrementing by `step`,
-            up to and including `stop`
-        '''
-        return None
+        if step != 0:
+            result = []
+            for value in range(start, stop + 1, step):
+                result.append(value)
+            return result
+        return [0]
 
     def get_even_list(self, start, stop, step):
-        ''' TODO - Implement solution
-        Given 3 integers,
-            `start`, `stop`, and `step`
-        return a list of all integers
-            between `start`,
-            incrementing by `step`,
-            up to and including `stop`
-            and are divisible by 2
-        '''
-        return None
+        if step != 0:
+            result = []
+            for value in range(start, stop + 1, step):
+                if value % 2 == 0:
+                    result.append(value)
+            return result
+        return [0]
 
     def get_odd_list(self, start, stop, step):
-        ''' TODO - Implement solution
-        Given 3 integers,
-            `start`, `stop`, and `step`
-        return a list of all integers
-            between `start`,
-            incrementing by `step`,
-            up to and including `stop`
-            and are not divisible by 2
-        '''
-        return None
+        if step != 0:
+            return [value for value in range(start, stop + 1, step) if value % 2 != 0]
+        return []
